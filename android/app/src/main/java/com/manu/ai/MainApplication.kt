1eb64ae2342900add5eed895d0c5b04f4ddf71f6
YOUR_GITHUB_USERNAME <YOUR_EMAIL>
2026-06-17 10:04:04 +0530

    fix: correct MainApplication with custom packages

--- android/app/src/main/java/com/manu/ai/MainApplication.kt
+++ android/app/src/main/java/com/manu/ai/MainApplication.kt
@@ -6,31 +6,27 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
     override val reactNativeHost: ReactNativeHost =
         object : DefaultReactNativeHost(this) {
-            override fun getPackages(): List<ReactPackage> =
-                PackageList(this).packages.apply {
-                    add(AppIntentsPackage())
-                    add(SpeechRecognitionPackage())
-                    add(NotificationPackage())
-                    add(AccessibilityPackage())
-                    add(UsageStatsPackage())
-                }
-
+            override fun getPackages(): List<ReactPackage> {
+                val packages = PackageList(this).packages
+                packages.add(AppIntentsPackage())
+                packages.add(SpeechRecognitionPackage())
+                packages.add(NotificationPackage())
+                packages.add(AccessibilityPackage())
+                packages.add(UsageStatsPackage())
+                return packages
+            }
             override fun getJSMainModuleName(): String = "index"
-
             override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+            override val isNewArchEnabled: Boolean = false
+            override val isHermesEnabled: Boolean = true
         }
 
     override val reactHost: ReactHost
@@ -38,9 +34,6 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, OpenSourceMergedSoMapping)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            load()
-        }
+        SoLoader.init(this, false)
     }
 }
